9b965c023335d13917bb2d7612bfaeb6eb1a5ced
Tiago Gadonski Cordeiro <tiagogadonski@hotmail.com>
2024-09-27 13:51:34 -0300

    Notes

---
+++ Crud.Server/Models/Note.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Crud.Server.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public List<string> Tags { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
